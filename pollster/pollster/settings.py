13ef7d7e508aa6544617ebf96b6e8a26d82e8bd4
yutveg <kodi.0n31l@gmail.com>
2020-07-15 14:09:52 -0700

    initializing templates

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c038l%#spo2cl8x*i9l%((ybbe!-msc#ho#(agglukp76mbiz@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
