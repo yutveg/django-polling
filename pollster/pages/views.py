fab21c958eae8bbd58a3c1e2832bf084d8262c08
yutveg <kodi.0n31l@gmail.com>
2020-07-17 14:12:56 -0700

    template tags and context

--- pollster/pages/views.py
+++ pollster/pages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    print(request.user)
-    return render(request, "pages/home.html", {})
+    context = {
+        "test": "this is a standard key",
+        "test_list": [12, 42, 35, 2, 1]
+    }
+    return render(request, "pages/home.html", context)
